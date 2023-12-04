ca594aaeceaf1c5d7d82e8f72e692cd0c3358d16
BarbareKereselidze <barbarekereselidze17@gmail.com>
2023-12-04 19:38:40 +0400

    Add config file

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from fake_csv_data_generator import GenerateFakeCsv
 from csv_to_json import ProcessAndStoreData
+from config_reader import read_config_file
 
 
 def main():
 
-    generator = GenerateFakeCsv()
-    generator.generate_csv("csv_files")
+    file_path = 'config.ini'
+    csv_path, json_path = read_config_file(file_path)
 
-    process_date = ProcessAndStoreData("csv_files", "json_files")
+    generator = GenerateFakeCsv()
+    generator.generate_csv(csv_path)
+
+    process_date = ProcessAndStoreData(csv_path, json_path)
     process_date.keep_data_in_json()
 
 
