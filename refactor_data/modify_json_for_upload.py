951c1cbeb7f3ef5e39afe2836b1b0648276cfa0e
BarbareKereselidze <barbarekereselidze17@gmail.com>
2023-12-13 18:11:06 +0400

    Add docstrings and comments

--- refactor_data/modify_json_for_upload.py
+++ refactor_data/modify_json_for_upload.py
@@ -5,10 +5,16 @@ from datetime import datetime
 class JsonDataModifier:
     @staticmethod
     def modify_json(json_file_path, data_file_path):
+        """ modifies a json file, adding timestamp information and creating a new adjusted json file """
+
+        # get the current date and time
         current_datetime = datetime.now()
         formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
 
+        # load json data from the input file
         json_data = json.load(open(json_file_path))
+
+        # adjust the json data by adding timestamp information
         adjusted_json_data = [
             {
                 'csv_file_name': list(row.keys())[0],
@@ -19,5 +25,6 @@ class JsonDataModifier:
             for row in json_data
         ]
 
+        #  write the adjusted json data to the output file
         with open(data_file_path, "w") as adjusted_file:
             adjusted_file.write('\n'.join(json.dumps(row) for row in adjusted_json_data))
